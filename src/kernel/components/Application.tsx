8c422f99d347adfd2f17826d8a7fdbe39dcc0c55
dvrmp <devrampe@gmail.es>
2022-04-21 10:49:37 +0200

    configured redux-toolkit

--- src/kernel/components/Application.tsx
+++ src/kernel/components/Application.tsx
@@ -1,7 +1,9 @@
-import { FC, Fragment } from "react";
+import { FC } from "react";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 export const Application: FC = () => {
-    return <Fragment>
+    return <Provider store={store}>
         <h1>COMPONENT: APPLICATION = MOUNTED</h1>
-    </Fragment>
+    </Provider>
 }
